cbc7454c7ea577e0c08eacab28960eba4cb96306
nazliozcelik <nazliozelikk@icloud.com>
2022-06-20 16:54:24 +0300

    implementing kubernetes cluster

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyCoversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyCoversionController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,16 @@ import java.util.HashMap;
 @RestController
 public class CurrencyCoversionController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyCoversionController.class);
+
     @Autowired
     private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+
+        //CHANGE-KUBERNETES
+        logger.info("calculateCurrencyConversion called with {} to {} with {}", from,to, quantity);
 
         HashMap<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
@@ -31,6 +38,8 @@ public class CurrencyCoversionController {
     @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
 
+        //CHANGE-KUBERNETES
+        logger.info("calculateCurrencyConversion called with {} to {} with {}", from,to, quantity);
 
         CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 
